057d1f7dd99150c32ca8b8dc8ae0dd950c030766
Lars van Erp <Larsverp2803@gmail.com>
2022-10-10 21:17:30 +0200

    Converted to statefull widget and added a key

--- lib/widgets/graph/GraphBar.dart
+++ lib/widgets/graph/GraphBar.dart
@@ -2,18 +2,23 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 
-class GraphBar extends StatelessWidget {
+class GraphBar extends StatefulWidget {
   int hour;
   int barHeight;
-
   int timeMachine;
 
   GraphBar({
+    Key? key,
     required this.hour,
     required this.timeMachine,
     required this.barHeight,
-  });
+  }) : super(key: key);
 
+  @override
+  State<GraphBar> createState() => _GraphBarState();
+}
+
+class _GraphBarState extends State<GraphBar> {
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -22,17 +27,17 @@ class GraphBar extends StatelessWidget {
         children: [
           Container(
             width: 9,
-            height: barHeight.toDouble(),
+            height: widget.barHeight.toDouble(),
             decoration: BoxDecoration(
-              color: barHeight > 40 && barHeight < 80
-                  ? timeMachine < 0
+              color: widget.barHeight > 40 && widget.barHeight < 80
+                  ? widget.timeMachine < 0
                       ? const Color(0xFF365651).withOpacity(0.2)
                       : const Color(0xFF365651)
-                  : barHeight > 70
-                      ? timeMachine < 0
+                  : widget.barHeight > 70
+                      ? widget.timeMachine < 0
                           ? const Color(0xFFEE5D1A).withOpacity(0.2)
                           : const Color(0xFFEE5D1A)
-                      : timeMachine < 0
+                      : widget.timeMachine < 0
                           ? const Color(0xFF00A963).withOpacity(0.2)
                           : const Color(0xFF00A963),
               borderRadius: BorderRadius.circular(4),
@@ -41,7 +46,7 @@ class GraphBar extends StatelessWidget {
           SizedBox(
             height: 8,
           ),
-          timeMachine == 0
+          widget.timeMachine == 0
               ? Text(
                   'nu',
                   style: TextStyle(
@@ -50,7 +55,7 @@ class GraphBar extends StatelessWidget {
                   ),
                 )
               : Text(
-                  hour.toString(),
+                  widget.hour.toString(),
                   style: TextStyle(
                     fontWeight: FontWeight.w200,
                     fontSize: 13,
