eadbfea273ddee9f6639d4b8f9423bd320ed7820
Lars van Erp <Larsverp2803@gmail.com>
2022-10-10 21:17:50 +0200

    Cleaned up some stuff and added keys to GrapBars

--- lib/widgets/graph/Graph.dart
+++ lib/widgets/graph/Graph.dart
@@ -6,17 +6,19 @@ import 'package:zonne_plan/widgets/graph/GraphBar.dart';
 class Graph extends StatelessWidget {
   final List<GraphBar> _previousBars = [];
   final List<GraphBar> _futureBars = [];
-  final ScrollController _scrollController = ScrollController(
-    initialScrollOffset: 380.0,
-    keepScrollOffset: true,
-  );
   final List<int> _powerCosts = [];
+  final Key _currentHourKey = UniqueKey();
 
   Graph() {
     int _previousGraphHight = 40;
     for (int i = 0; i < 48; i++) {
       _previousGraphHight = Random().nextInt(_previousGraphHight + 20) +
           (_previousGraphHight / 4).toInt();
+
+      _previousGraphHight > 140
+          ? _previousGraphHight = 140
+          : _previousGraphHight;
+
       _powerCosts.add(_previousGraphHight);
     }
 
@@ -25,6 +27,7 @@ class Graph extends StatelessWidget {
         hour: DateTime.now().add(Duration(hours: i)).hour,
         timeMachine: -1,
         barHeight: _powerCosts[i + 23],
+        key: UniqueKey(),
       ));
     }
 
@@ -33,6 +36,7 @@ class Graph extends StatelessWidget {
         hour: DateTime.now().add(Duration(hours: i)).hour,
         timeMachine: 1,
         barHeight: _powerCosts[i + 23],
+        key: UniqueKey(),
       ));
     }
   }
@@ -41,19 +45,24 @@ class Graph extends StatelessWidget {
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       scrollDirection: Axis.horizontal,
-      controller: _scrollController,
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          ..._previousBars,
-          GraphBar(
-            hour: DateTime.now().hour,
-            timeMachine: 0,
-            barHeight: _powerCosts[23],
-          ),
-          ..._futureBars,
-        ],
+      child: ConstrainedBox(
+        constraints: const BoxConstraints(
+          minHeight: 170,
+        ),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            ..._previousBars,
+            GraphBar(
+              hour: DateTime.now().hour,
+              timeMachine: 0,
+              barHeight: _powerCosts[23],
+              key: _currentHourKey,
+            ),
+            ..._futureBars,
+          ],
+        ),
       ),
     );
   }
